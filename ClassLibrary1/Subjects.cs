5730794e77e18bf3ed1bfe10fec2cfaade7862eb
Egor <79940875+Bareen86@users.noreply.github.com>
2021-04-09 20:08:18 +0300

    Create Subjects.cs

---
+++ ClassLibrary1/Subjects.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityClass
+{
+    class Subjects
+    {
+        public string FirstSubject;
+        public string SecondSubject;
+        public string ThirdSubject;
+        public string FourthSubject;
+        public string FifthSubject;
+    }
+}
