39afeb1a0fe014da8313805d33610f3d42740a02
Egor <79940875+Bareen86@users.noreply.github.com>
2021-04-09 20:08:13 +0300

    Create Student.cs

---
+++ ClassLibrary1/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityClass
+{
+    class Student
+    {
+        public string FirstName;
+        public string LastName;
+        public string MiddleName;
+        public int Age;
+        public string Group;
+    }
+}
