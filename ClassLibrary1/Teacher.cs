773edf5dc680441db38bca416a88104f1f767b07
Egor <79940875+Bareen86@users.noreply.github.com>
2021-04-09 20:08:22 +0300

    Create Teacher.cs

---
+++ ClassLibrary1/Teacher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityClass
+{
+    class Teacher
+    {
+        public string FirstName;
+        public string LastName;
+        public string MiddleName;
+        public int Age;
+        public string Subject;
+    }
+}
